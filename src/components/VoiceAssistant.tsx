897723d4c56e4c4ad912799a7a095ec843b61183
usman1058 <techinfo1058@gmail.com>
2025-09-13 20:01:40 +0500

    task_pro_org_v2

--- src/components/VoiceAssistant.tsx
+++ src/components/VoiceAssistant.tsx
@@ -221,13 +221,13 @@ const VoiceAssistant = () => {
       {/* Controls - Fixed at bottom */}
       <div className="p-4 border-t border-gray-200 flex-shrink-0">
         {/* Error display */}
-        {(micError || error) && (
+        {micError && (
           <div className="mb-3 p-3 bg-red-50 border border-red-200 rounded-lg">
             <div className="text-red-700 text-sm flex items-start">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-1 flex-shrink-0" viewBox="0 0 20 20" fill="currentColor">
                 <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
               </svg>
-              <span>{micError || error}</span>
+              <span>{micError}</span>
             </div>
           </div>
         )}
